62d0f9c940edfe300f1b10b2366dd5410d9c35cd
freamwork97 <jsy11123@naver.com>
2023-08-18 07:30:54 +0900

    python_TIS

--- 07_selenium/ex03_selenium.py
+++ 07_selenium/ex03_selenium.py
@@ -1,20 +1,12 @@
-###########################################
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-###########################################
 
-# 크롬 실행
+# 크롬 브라우저 실행
 driver = webdriver.Chrome()
-
 # 접속할 주소
-driver.get("https://selenium.dev")
+driver.get("https://www.selenium.dev/")
 
-# id = main_vavber요소 접근
-id_element = driver.find_element(By.ID, "main_naver")
-
-
+# id=main_navbar 요소 접근
+id_element = driver.find_element(By.ID, 'main_navbar')
 print(id_element.text)
-
-# 10초 동안 현재 상태에서 대기
-time.sleep(30)
