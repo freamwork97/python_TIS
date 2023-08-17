2b3302e7b4e899e8683aa96cdea1ffca96307c42
freamwork97 <jsy11123@naver.com>
2023-08-17 09:05:03 +0900

    python_TIS

--- 04_functions/05_fun_ex.py
+++ 04_functions/05_fun_ex.py
@@ -1,18 +1,30 @@
 # 실행하면 콘솔에서 1또는2를 입력받고 1은 세로형구구단, 2는 가로형구구단을 각각 출력한다.
 # 구구단은 각각 함수로 정의하도록 한다.
 
+def mul1():
+    for i in range(2, 10):
+        for j in range(1, 10):
+            print(i, "x", j, "=", i*j)
 
-def mul(num):
+
+def mul2():
+    for i in range(2, 10):
+        for j in range(1, 10):
+            print(i, "x", j, "=", i*j, end=" ")
+        print()
+
+
+run = True
+
+while run:
+    num = int(input("숫자입력 : "))
+
     if num == 1:
-        for i in range(2, 10):
-            for j in range(1, 10):
-                print(i, "x", j, "=", i*j)
+        print("1번")
+        mul1()
     elif num == 2:
-        for i in range(2, 10):
-            for j in range(1, 10):
-                print(i, "x", j, "=", i*j, end=" ")
-            print()
-
-
-num = int(input("숫자를 입력 : "))
-mul(num)
+        print("2")
+        mul2()
+    elif num == 3:
+        print("종료")
+        break
