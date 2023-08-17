d9eb2ab60bc44be34483640b29cc587d28cc3d1b
freamwork97 <jsy11123@naver.com>
2023-08-18 07:36:02 +0900

    Python

--- 07_selenium/ex07_webtoon.py
+++ 07_selenium/ex07_webtoon.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 ###########################################
 
@@ -11,7 +13,11 @@ driver = webdriver.Chrome()
 
 # 접속할 주소
 driver.get('https://comic.naver.com/webtoon')
-time.sleep(5)
+# time.sleep(5)
+
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, '[class="text"]')))
 
 webtoon = driver.find_elements(By.CSS_SELECTOR, '[class="text"]')
 
