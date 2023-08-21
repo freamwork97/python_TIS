2afa51c6c2c4cc428ddd3c2a4b4661cef16d56c4
freamwork97 <jsy11123@naver.com>
2023-08-21 09:12:50 +0900

    python_TIS

--- 08_youtube/ex09_title_hits.py
+++ 08_youtube/ex09_title_hits.py
@@ -41,7 +41,7 @@ def scroll_fun():
 driver = webdriver.Chrome()
 
 # 접속할 주소
-driver.get('https://www.youtube.com/')
+driver.get('https://www.youtube.com/feed/trending')
 time.sleep(2)
 
 # 무한스크롤 함수 호출
@@ -50,16 +50,34 @@ scroll_fun()
 # 제목 요소 접근
 titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
 
+# 제목 저장 리스트
+title_list = []
+# 조회수 저장 리스트
+hits_list = []
+
 for i in titles:
     if i.get_attribute("aria-label") and i.text:  # shorts 영상을 걸러내기 위한 조건문
         # aria_label 속성값 가져오기
         aria_label = i.get_attribute("aria-label")
 
-        print(aria_label)
+        # print(aria_label)
 
         start_index = aria_label.rfind("조회수")+4
         end_index = aria_label.rfind("회")
         hits = aria_label[start_index:end_index]
         hits = int(hits.replace(",", ""))
-        print('제목', i.text)
-        print('조회수', hits)
+        # print('제목', i.text)
+        # print('조회수', hits)
+
+        # 제목, 조회수 리스트에 담기
+        # append() 리스트에 데이터 추가
+        title_list.append(i.text)
+        hits_list.append(hits)
+
+# 리스트 데이터 확인
+# print('제목리스트\n', title_list)
+# print('조회수리스트\n', hits_list)
+
+# 제목, 조회수 리스트 함께 조회
+for t, h in zip(title_list, hits_list):
+    print(t, h)
