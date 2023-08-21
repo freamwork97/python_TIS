a60c428e5bb31e5ee889a302e38be9836ac2f0f0
freamwork97 <jsy11123@naver.com>
2023-08-21 11:16:10 +0900

    python_TIS

--- 09_pandas/ex03_pandas.py
+++ 09_pandas/ex03_pandas.py
@@ -46,3 +46,59 @@ scores_dict = {
 scores = pd.DataFrame(scores_dict)
 
 print(scores)
+
+
+scores = pd.DataFrame(
+    {
+        "java": [96, 76, 80, 64, 85],  # java
+        "python": [88, 90, 100, 40, 50],  # python
+        'js': [10, 20, 30, 50, 60]  # js
+    },
+    index=student_number
+)
+print(scores)
+
+# 이름 데이터 추가
+scores['이름'] = ['김파이', '이파이', '빅파이', '스몰파이', '미드파이']
+print(scores)
+
+# 데이터 추가
+scores.loc[6] = [80, 80, 80, '조파이']
+print(scores)
+
+# 학번, 이름, 성적 모두 포함한 DataFrame선언
+
+student_number = [1, 2, 3, 4, 5, 6]
+
+scores = pd.DataFrame(
+    {
+        '이름': ['생선', '양고기', '닭고기', '나비탕', '곰탕', '보신탕'],
+        "java": [96, 76, 80, 64, 85, 86],  # java
+        "python": [88, 90, 100, 40, 50, 92],  # python
+        'js': [10, 20, 30, 50, 60, 100]  # js
+    },
+    index=student_number
+)
+print(scores)
+
+# index 기준 정렬
+print(scores.sort_index())
+# index 기준 내림차순
+print(scores.sort_index(ascending=False))
+
+# '이름' 열 기준 오름차순 정렬
+print(scores.sort_values(by='이름', ascending=True))
+# '이름' 열 기준 내림차순 정렬
+print(scores.sort_values(by='이름', ascending=False))
+# python 기준 오름차순 정렬
+print(scores.sort_values(by='python', ascending=True))
+# python 기준 내림차순 정렬
+print(scores.sort_values(by='python', ascending=False))
+
+# 첫 2줄만 조회
+print(scores.head(2))
+# 마지막 2줄만 조회
+print(scores.tail(2))
+
+# DataFrame을 csv로 내보내기
+scores.to_csv('./09_pandas/scores.csv', encoding='utf-8-sig')
